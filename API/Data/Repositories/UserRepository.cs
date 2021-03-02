76f71e9240e0f1f872f1676b00c2661950338c19
Stefan Mitrović <stefmit10@gmail.com>
2021-03-02 21:49:16 +0100

    End of Section 13

--- API/Data/Repositories/UserRepository.cs
+++ API/Data/Repositories/UserRepository.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
+using API.Helpers;
 using API.Interfaces;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
@@ -29,7 +30,7 @@ namespace API.Data.Repositories
                 .SingleOrDefaultAsync();
         }
 
-        public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+        public async Task<IEnumerable<MemberDto>> GetMembersAsync(UserParams userParams)
         {
             return await _context.Users
                 .ProjectTo<MemberDto>(_mapper.ConfigurationProvider)
